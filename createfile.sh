f33c3c49ef436d86e6525648f6ef97a595c7d6c3
ignatiusbarry69 <ignatius.barry@ti.ukdw.ac.id>
2024-08-10 18:45:51 +0000

    genesis

---
+++ createfile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+touch frontend/karsajobs-ui-service.yml frontend/karsajobs-ui-deployment.yml
+touch mongodb/mongo-configmap.yml mongodb/mongo-secret.yml mongodb/mongo-pv-pvc.yml mongodb/mongo-service.yml mongodb/mongo-statefulset.yml
